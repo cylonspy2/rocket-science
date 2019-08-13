2d2d63f0d4b998ac0f48acb45074d1d37303b28e
Kadeo13 <kadeo13@gmail.com>
2019-08-13 15:06:22 -0400

    pause menu

---
+++ Rocket Science Build/Assets/Scripts/PauseMenu1.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu1 : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        GameIsPaused = false;
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        Cursor.visible = false;
+        Debug.Log("Hi");
+    }
+
+    void Pause()
+    {
+        GameIsPaused = true;
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        Cursor.visible = true;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game.");
+        Application.Quit();
+    }
+}
